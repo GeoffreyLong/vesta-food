a7969529b33e42bbba80116d84f71a0a135f6a70
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-05-12 03:34:59 -0400

    Attempt at upserting the email

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+var mongo = require('mongodb');
+
+
+mongoose.connect('mongodb://localhost/VestaFood', function (error) {
+  if (error) {
+      console.log(error);
+  }
+  else{
+    console.log('Successfully Connected');
+  }
+});
+
+var Schema = mongoose.Schema;
+
+
+var EarlyUserSchema = new Schema({
+    email: String,
+});
+
+var EarlyUsers = mongoose.model('earlyUsers', EarlyUserSchema);
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -10,8 +32,41 @@ router.get('/', function(req, res) {
  * Occurs when the user enters their email address on the splash page
  */
 router.post('/newsletter', function(req, res) {
-  var email = req.body.email;
-  console.log('User email address entered is ' + email);
+  var newEmail = req.body.email;
+  console.log('User email address entered is ' + newEmail);
+
+  // Upsert ensures that only adds if doesn't exist
+  EarlyUsers.update({email: newEmail}, 
+                    {upsert: true}, 
+                    {$setOnInsert: newEmail},
+                    function(err, saved) {
+    if (err){
+      console.log(err);
+      res.status(500).send(err);
+    }
+    console.log(saved);
+
+    // Should redirect to a second survey
+    // This survey would add additional information to the schema
+    // Updated via a find by email
+    res.redirect("/");
+  });
+  
+  /*
+  new EarlyUsers({
+    email: newEmail
+  }).save(function(err, saved){
+    if (err){
+      console.log(err);
+      res.status(500).send(err);
+    }
+
+    // Should redirect to a second survey
+    // This survey would add additional information to the schema
+    // Updated via a find by email
+    res.redirect("/");
+  });
+  */
 });
 
 module.exports = router;
