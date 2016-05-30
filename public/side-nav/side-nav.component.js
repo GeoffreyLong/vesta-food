1921c9e28485340b970734145b7768d7352ae170
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-05-30 05:20:21 -0400

    Add poor initial implementation of sidenav

--- public/side-nav/side-nav.component.js
+++ public/side-nav/side-nav.component.js
@@ -1,6 +1,8 @@
 angular.module('sideNav').component('sideNav', {
   templateUrl: 'side-nav/side-nav.template.html',
-  controller: function SideNavController() {
-
+  controller: function SideNavController($scope, $mdSidenav) {
+    $scope.openLeftMenu = function() {
+      $mdSidenav('left').toggle();
+    };
   }
 });
