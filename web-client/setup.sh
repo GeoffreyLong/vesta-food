96ce42f7b50bc39df3e950dda1a2cfd9d93b1677
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-06-13 09:31:24 -0400

    Add method to rewrite nginx file so it is almost platform independent

--- web-client/setup.sh
+++ web-client/setup.sh
@@ -2,6 +2,9 @@
 echo "hello, $USER in the directory: $PWD"
 echo "This is supposed to set up nginx"
 echo "For now just ask Geoff how to do it manually"
+rm $PWD/nginx/localhost
+cp $PWD/nginx/localhost_preproc $PWD/nginx/localhost
+sed -i -e 's|$rootloc|'$PWD'|g' $PWD/nginx/localhost
 sudo rm /etc/nginx/sites-enabled/localhost
 sudo ln -s $PWD/nginx/localhost /etc/nginx/sites-enabled/
 
