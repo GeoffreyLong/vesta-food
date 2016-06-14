c83b818ebc5e36c7b6da85b451865ae15dbeb726
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-06-14 11:02:35 -0400

    Initial seed elements for stores view

--- web-client/app/stores-view/stores-view.component.js
+++ web-client/app/stores-view/stores-view.component.js
@@ -3,14 +3,81 @@ angular.module('storesView').component('storesView', {
   controller: function StoresViewController() {
     this.stores = [
       {
-        name: 'Nexus S',
-        snippet: 'Fast just got faster with Nexus S.'
+        userID: "123456789",
+        storeID: "123456789",
+        storeTitle: "Geoff's Store 1",
+        personalPhoto: "TODO",
+        pickupAddress: "3515 Rue Durocher",
+        neighborhood: "TODO",
+        stripeAuth: "TODO",
+        description: "This is the best store you'll ever go to. I make lots of things.",
+        availability: "TODO",
+        overallRating: 4,
+        foods: [{
+          name: "Brittle Peanuts",
+          photo: "TODO",
+          price: 10,
+          shelfLife: 3,
+          prepTime: 2,
+          overallRating: 3
+        }]
+      },{
+        userID: "123456789",
+        storeID: "123456789",
+        storeTitle: "Geoff's Store 2",
+        personalPhoto: "TODO",
+        pickupAddress: "3515 Rue Durocher",
+        neighborhood: "TODO",
+        stripeAuth: "TODO",
+        description: "This is the best store you'll ever go to. I make lots of things.",
+        availability: "TODO",
+        overallRating: 4,
+        foods: [{
+          name: "Brittle Peanuts",
+          photo: "TODO",
+          price: 10,
+          shelfLife: 3,
+          prepTime: 2,
+          overallRating: 3
+        }]
       }, {
-        name: 'Motorola XOOM™ with Wi-Fi',
-        snippet: 'The Next, Next Generation tablet.'
+        userID: "123456789",
+        storeID: "123456789",
+        storeTitle: "Geoff's Store 3",
+        personalPhoto: "TODO",
+        pickupAddress: "3515 Rue Durocher",
+        neighborhood: "TODO",
+        stripeAuth: "TODO",
+        description: "This is the best store you'll ever go to. I make lots of things.",
+        availability: "TODO",
+        overallRating: 4,
+        foods: [{
+          name: "Brittle Peanuts",
+          photo: "TODO",
+          price: 10,
+          shelfLife: 3,
+          prepTime: 2,
+          overallRating: 3
+        }]
       }, {
-        name: 'MOTOROLA XOOM™',
-        snippet: 'The Next, Next Generation tablet.'
+        userID: "123456789",
+        storeID: "123456789",
+        storeTitle: "Geoff's Store 4",
+        personalPhoto: "TODO",
+        pickupAddress: "3515 Rue Durocher",
+        neighborhood: "TODO",
+        stripeAuth: "TODO",
+        description: "This is the best store you'll ever go to. I make lots of things.",
+        availability: "TODO",
+        overallRating: 4,
+        foods: [{
+          name: "Brittle Peanuts",
+          photo: "TODO",
+          price: 10,
+          shelfLife: 3,
+          prepTime: 2,
+          overallRating: 3
+        }]
       }
     ];
   }
