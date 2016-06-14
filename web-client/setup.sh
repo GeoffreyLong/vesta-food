17672c6e2d96c082325d6abf3c1a0137dda0e62b
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-06-14 14:27:07 -0400

    Move stylus compilation to client server

--- web-client/setup.sh
+++ web-client/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "hello, $USER in the directory: $PWD"
-echo "This is supposed to set up nginx"
-echo "For now just ask Geoff how to do it manually"
+
+####### NGINX #######
 rm $PWD/nginx/localhost
 cp $PWD/nginx/localhost_preproc $PWD/nginx/localhost
 sed -i -e 's|$rootloc|'$PWD'|g' $PWD/nginx/localhost
@@ -11,3 +11,9 @@ sudo ln -s $PWD/nginx/localhost /etc/nginx/sites-enabled/
 #TODO write and add one for production
 
 sudo nginx -s reload
+
+####### STYLUS #######
+# Compile and watch stylus stuff
+# TODO really only want to -w on development
+# Include this difference ith the production
+stylus -w -u nib $PWD/app/stylesheets/*.styl
