7b4321d7ef1b0b07e52adac7cbf809ab400aa4a9
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-06-14 12:25:31 -0400

    Make initial dynamic store card population

--- web-client/app/stores-view/stores-view.component.js
+++ web-client/app/stores-view/stores-view.component.js
@@ -1,6 +1,19 @@
 angular.module('storesView').component('storesView', {
   templateUrl: 'stores-view/stores-view.template.html',
   controller: function StoresViewController() {
+    function chunk(arr, size) {
+      var newArr = [];
+      for (var i = 0; i < size; i ++) {
+        var tempArr = [];
+        for (var j = i; j < arr.length; j += size) {
+          tempArr.push(arr[j]);
+        }
+        newArr.push(tempArr);
+      }
+      return newArr;
+    }
+
+
     this.stores = [
       {
         userID: "123456789",
@@ -78,7 +91,29 @@ angular.module('storesView').component('storesView', {
           prepTime: 2,
           overallRating: 3
         }]
+      }, {
+        userID: "123456789",
+        storeID: "123456789",
+        storeTitle: "Geoff's Store 5",
+        personalPhoto: "TODO",
+        pickupAddress: "3515 Rue Durocher",
+        neighborhood: "TODO",
+        stripeAuth: "TODO",
+        description: "This is the best store you'll ever go to. I make lots of things.",
+        availability: "TODO",
+        overallRating: 4,
+        foods: [{
+          name: "Brittle Peanuts",
+          photo: "TODO",
+          price: 10,
+          shelfLife: 3,
+          prepTime: 2,
+          overallRating: 3
+        }]
       }
     ];
+
+    this.chunkedData = chunk(this.stores, 2);
+
   }
 });
