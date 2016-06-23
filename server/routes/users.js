ed6f0926ba4c072b07844a325bcbbf1e2bf27e5f
Charlie Bloomfield <charlielbloomfield@gmail.com>
2016-06-23 17:50:22 -0400

    added stripe auth

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,59 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
+var Users = require('../models/user');
+var Store = require('../models/store');
+
+var STRIPE_CLIENT_ID = 'ca_89GeQlhMVoUResS0PeQm6XuCs6hoXgze';
+var STRIPE_API_KEY = 'sk_test_an3Nezne8XguJAefiBJgNV63';
+var STRIPE_TOKEN_URI = 'https://connect.stripe.com/oauth/token';
+
+
+//
 router.get('/:id', function(req, res) {
 
 });
 
-// update to seller
-// TODO needs different UPDATE endpoint
-router.put('/:id/', function(req, res) {
-  console.log(username + ':' + req.params.scope + ':' + req.params.code);
 
-  // TODO stripe api
-  // send current user
-  res.json(200);
+//
+router.post('/becomeChef', function(req, res) {
+  console.log(req.body.scope + ' : ' + req.body.code);
+
+  request.post({
+    url: STRIPE_TOKEN_URI,
+    form: {
+      grant_type: 'authorization_code',
+      client_id: STRIPE_CLIENT_ID,
+      client_secret: STRIPE_API_KEY,
+      code: req.body.code
+    }
+  }, function (stripeError, response, body) {
+    if (!stripeError) {
+      console.log(body);
+
+      var store = new Store({
+        stripeCredentials: JSON.parse(body)
+      });
+
+      store.save(function (storeError) {
+        if (!storeError) {
+          res.status(200).send();
+        }
+        else {
+          console.log('mongoose error creating store');
+          console.log(storeError);
+          res.status(500).send(storeError);
+        }
+      })
+    }
+
+    else {
+      console.log('stripe error processing client id');
+      console.log(stripeError);
+      res.status(500).send(stripeError);
+    }
+  })
 });
 
 module.exports = router;
