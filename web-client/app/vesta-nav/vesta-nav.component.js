3694586c0737f8ae184ed579a507c079d7e1adb6
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-06-25 16:05:11 -0400

    Interim attempt at fixing the nav dropdown

--- web-client/app/vesta-nav/vesta-nav.component.js
+++ web-client/app/vesta-nav/vesta-nav.component.js
@@ -1,6 +1,6 @@
 angular.module('vestaNav').component('vestaNav', {
   templateUrl: 'vesta-nav/vesta-nav.template.html',
-  controller: function VestaNavController($scope, $mdSidenav, authService, $location) {
+  controller: function VestaNavController($scope, $mdSidenav, authService, $location, $mdPanel, mdPanelRef) {
     this.session = authService.getSession();
     this.currentPath = $location.path();
 
@@ -19,6 +19,28 @@ angular.module('vestaNav').component('vestaNav', {
       authService.logout();
     }
 
+    $scope.openMenu = function(ev){
+      var position = $mdPanel.newPanelPosition()
+          .relativeTo('.menuOpenButton')
+          .addPanelPosition($mdPanel.xPosition.ALIGN_START, $mdPanel.yPosition.BELOW);
+      var config = {
+        attachTo: angular.element(document.body),
+        controller: function() {},
+        controllerAs: 'ctrl',
+        template: '<md-menu-content>'
+      + '<md-menu-item class="md-indent"><md-icon class="material-icons"> person </md-icon><md-button ng-click="logout()">Logout</md-button></md-menu-item>'
+      +  '<md-menu-divider></md-menu-divider><md-menu-item class="md-indent"><md-button ng-click="">Unrelated actions</md-button></md-menu-item>'
+      + '</md-menu-content>',
+        panelClass: 'demo-menu-example',
+        position: position,
+        openFrom: ev,
+        clickOutsideToClose: true,
+        escapeToClose: true,
+        focusOnOpen: true,
+        zIndex: 12
+      };
+      $mdPanel.open(config);
+    };
 
     /********************* SIDE NAV *************************************/
     // TODO
