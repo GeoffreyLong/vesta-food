859e35a5fcc318bc123a73f6c1e1243cf70999e7
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-06-25 16:27:43 -0400

    Fix the navbar dropdown

--- web-client/app/vesta-nav/vesta-nav.component.js
+++ web-client/app/vesta-nav/vesta-nav.component.js
@@ -1,9 +1,10 @@
 angular.module('vestaNav').component('vestaNav', {
   templateUrl: 'vesta-nav/vesta-nav.template.html',
-  controller: function VestaNavController($scope, $mdSidenav, authService, $location, $mdPanel, mdPanelRef) {
+  controller: function VestaNavController($scope, $mdSidenav, authService, $location, $mdMenu) {
     this.session = authService.getSession();
     this.currentPath = $location.path();
 
+    // TODO issues when clicking escape... capture this too
     $scope.openLeftMenu = function() {
       $mdSidenav('left').toggle();
 
@@ -19,28 +20,10 @@ angular.module('vestaNav').component('vestaNav', {
       authService.logout();
     }
 
-    $scope.openMenu = function(ev){
-      var position = $mdPanel.newPanelPosition()
-          .relativeTo('.menuOpenButton')
-          .addPanelPosition($mdPanel.xPosition.ALIGN_START, $mdPanel.yPosition.BELOW);
-      var config = {
-        attachTo: angular.element(document.body),
-        controller: function() {},
-        controllerAs: 'ctrl',
-        template: '<md-menu-content>'
-      + '<md-menu-item class="md-indent"><md-icon class="material-icons"> person </md-icon><md-button ng-click="logout()">Logout</md-button></md-menu-item>'
-      +  '<md-menu-divider></md-menu-divider><md-menu-item class="md-indent"><md-button ng-click="">Unrelated actions</md-button></md-menu-item>'
-      + '</md-menu-content>',
-        panelClass: 'demo-menu-example',
-        position: position,
-        openFrom: ev,
-        clickOutsideToClose: true,
-        escapeToClose: true,
-        focusOnOpen: true,
-        zIndex: 12
-      };
-      $mdPanel.open(config);
-    };
+    $scope.openMenu = function(open, ev) {
+      open();
+      $('body').css({'background': 'relative'});
+    }
 
     /********************* SIDE NAV *************************************/
     // TODO
