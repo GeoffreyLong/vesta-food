e20d3eac46a04cdc9b96aa057c1e2e714a60d2e7
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-06-27 16:51:51 -0400

    Initial commit of new splash

--- routes/common.js
+++
@@ -1,8 +0,0 @@
-// A file for commonly used functions
-
-var env = require('../env.json');
-
-exports.config = function() {
-  var node_env = process.env.NODE_ENV || 'development';
-  return env[node_env];
-};
