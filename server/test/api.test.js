93427d89b44be8a90d787d8ecd3b30b4f9b3dbe3
Charlie Bloomfield <charlielbloomfield@gmail.com>
2016-07-07 15:55:50 -0400

    ADD: chai to dependencies and chai exmaple test

--- server/test/api.test.js
+++ server/test/api.test.js
@@ -1,11 +1,25 @@
 // var seed = require('./init_db');
 var request = require('supertest');
+var assert = require('chai').assert;
 var app = require('../src/app');
 
-describe("authentication endpoint", function () {
+describe("status endpoint", function () {
   it("returns status 200", function (done) {
     request(app)
       .get("/api/status")
       .expect(200, done);
+  });
+});
+
+describe("store endpoint", function () {
+  it("returns status 200", function (done) {
+    request(app)
+      .get("/api/stores")
+      .expect(200)
+      .end(function (err, res) {
+        var stores = res.body;
+        assert.lengthOf(stores, 0);
+        done();
+      });
   });
 });
