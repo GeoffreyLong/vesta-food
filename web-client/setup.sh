88737fb73b0f70eeeba40d793c297986ebc7d8db
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-07-07 16:37:33 -0400

    Revert changes to the photo server changes
    Decided that it would be better to have all nginx served content in one location, no matter if it comes from server side

--- web-client/setup.sh
+++ web-client/setup.sh
@@ -4,9 +4,7 @@ echo "hello, $USER in the directory: $PWD"
 ####### NGINX #######
 rm $PWD/nginx/localhost
 cp $PWD/nginx/localhost_preproc $PWD/nginx/localhost
-sed -i -e 's|$appRoot|'$PWD'|g' $PWD/nginx/localhost
-parent_path=$( cd "../$(dirname "${BASH_SOURCE}")/server" ; pwd -P )
-sed -i -e 's|$serverRoot|'$parent_path'|g' $PWD/nginx/localhost
+sed -i -e 's|$rootloc|'$PWD'|g' $PWD/nginx/localhost
 sudo rm /etc/nginx/sites-enabled/localhost
 sudo ln -s $PWD/nginx/localhost /etc/nginx/sites-enabled/
 
