e138df0e15bbca8af3e5750dc5fb4c7520be8672
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-07-14 19:18:13 -0400

    Intermediate commit for possible auth approach

--- web-client/app/services/auth-service.js
+++ web-client/app/services/auth-service.js
@@ -20,38 +20,68 @@ angular
         });
       }
 
-      function getSession(requireUser) {
-        if (session && (session.address || (session.user && session.user._id))) {
-          return session;
+      function getSession(requireSession, requireUser, requireStore) {
+        var deferred = $q.defer();
+        if (session && (requireSession && (session.address || requireUser())
+                    && (requireUser && requreUser()) 
+                    && (requireStore && requireStore())){
+          deferred.resolve(session);
         }
         else {
-          var deferred = $q.defer();
-
-          $http.get("/api/auth/session").then(function (result) {
-            if ((!requireUser && result.data.address)
-              || (result.data.user && result.data.user._id)) {
-              session = result.data;
+          getSessionFromDB().then(function(err){
+            deferred.reject({acquired: false});
+          }, function(session){
+            if (session && (requireSession && (session.address || requireUser())
+                        && (requireUser && requreUser()) 
+                        && (requireStore && requireStore())){
               deferred.resolve(session);
-              // TODO could speed up using session storage
-              //    $window.sessionStorage["userInfo"] = JSON.stringify(userInfo);
             }
-            else {
-              if (requireUser) deferred.reject({authenticated: false});
-              deferred.reject({sessioned: false});
-            }
-          }, function (error) {
-            // TODO redirect to "Temporary Maintanence page or something"
-            console.log("server out");
-            deferred.reject({sessioned: false});
           });
-
-          return deferred.promise
         }
+        return deferred.promise;
       }
 
-      function requireStore() {
+
+      function getSessionFromDB(){
+        var deferred = $q.defer();
+
+        $http.get("/api/auth/session").then(function (result) {
+          if ((!requireUser && result.data.address)
+            || (result.data.user && result.data.user._id)) {
+            session = result.data;
+            deferred.resolve(session);
+            // TODO could speed up using session storage
+            //    $window.sessionStorage["userInfo"] = JSON.stringify(userInfo);
+          }
+          else {
+            if (requireUser) deferred.reject({authenticated: false});
+            deferred.reject({sessioned: false});
+          }
+        }, function (error) {
+          // TODO redirect to "Temporary Maintanence page or something"
+          console.log("server out");
+          deferred.reject({sessioned: false});
+        });
+
+        return deferred.promise;
+      }
+
+      function requireSession(){
+        
+      }
+
+      function requireUser(){
+
+
+
+        return deferred.promse;
+      }
+
+      function requireStore(){
         // NOTE might not be the best way to handle the path...
         //      Seems rather shotty to me...
+        // TODO make this extensible to other things needing a store
+        //      put the matching paths regex in private fn?
         var re = new RegExp("\/store\/(.*)\/edit");
         var storeId = re.exec($location.path())[1];
 
@@ -62,25 +92,21 @@ angular
             && (session.user.storeId == storeId)) {
             return session;
           }
-          else {
-            return null;
+        }
+
+        var deferred = $q.defer();
+        getSession(true).then(function(session){
+          if (session && session.user && storeId
+            && (session.user.storeId == storeId)) {
+            deferred.resolve(session)
           }
-        }
-        else {
-          var deferred = $q.defer();
-          getSession(true).then(function(session){
-            if (session && session.user && storeId
-              && (session.user.storeId == storeId)) {
-              deferred.resolve(session)
-            }
-            else {
-              return deferred.reject({storeOwner: false});
-            }
-          }, function(err) {
+          else {
             return deferred.reject({storeOwner: false});
-          });
-          return deferred.promise;
-        }
+          }
+        }, function(err) {
+          return deferred.reject({storeOwner: false});
+        });
+        return deferred.promise;
       }
 
       function init() {
