e986e650f8e22d8b9449f463f3cc915446209478
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-07-25 20:57:56 -0400

    ADD initial user profile view

--- web-client/app/profile/profile.component.js
+++ web-client/app/profile/profile.component.js
@@ -1,6 +1,21 @@
 angular.module('profile').component('profile', {
   templateUrl: 'profile/profile.template.html',
-  controller: function ProfileController($scope) {
+  controller: function ProfileController($scope, $http, $location, authService) {
+    var re = new RegExp("\/user\/(.*)");
+    var userId = re.exec($location.path())[1];        
+    if (!$scope.user){
+      $http.get('/api/users/' + userId).then(function(user) {
+        $scope.user = user.data;
+        console.log($scope.user);
+      }, function(err) {
+        
+      });
+    }    
 
+    $scope.checkOwnership = function() {
+      var session = authService.getSessionSynch();
+      return (session && session.user && $scope.user
+              && (session.user._id === $scope.user._id));
+    }    
   }
 });
